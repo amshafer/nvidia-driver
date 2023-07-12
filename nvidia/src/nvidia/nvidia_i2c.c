f632d580efe08a44d6a4c8f0f38b48b3459010d5
Austin Shafer <ashafer@badland.io>
2023-07-12 13:40:09 -0400

    Populate 535.54.03

---
+++ nvidia/src/nvidia/nvidia_i2c.c
@@ -0,0 +1,11 @@
+#include "os-interface.h"
+#include "nv.h"
+
+void* NV_API_CALL nv_i2c_add_adapter(nv_state_t *nv, NvU32 port)
+{
+    return NULL;
+}
+
+void NV_API_CALL nv_i2c_del_adapter(nv_state_t *nv, void *data)
+{
+}
